c990df7a924fe5210fa504ee2f0e5546e315394c
Ka Hung Lee <lee.kahung414@gmail.com>
2025-02-19 03:12:42 -0800

    Update styling for TableLoading; Refactor Member page

--- src/global/components/TableLoading.tsx
+++ src/global/components/TableLoading.tsx
@@ -1,6 +1,10 @@
-export default function TableLoading(): React.JSX.Element {
+interface Props {
+  small?: boolean;
+}
+
+export default function TableLoading({ small }: Props): React.JSX.Element {
   return (
-    <tr className="h-[calc(100vh-80px-250px)]">
+    <tr className={`${small ? 'h-[calc(100vh-80px-250px-250px)]' : 'h-[calc(100vh-80px-250px)]'}`}>
       <td colSpan={6}>
         <div className="flex items-center justify-center">
           Fetching Data...
--- src/pages/Member/Member.tsx
+++ src/pages/Member/Member.tsx
@@ -8,9 +8,9 @@ export default function Member(): React.JSX.Element {
 
   return (
     <div className="flex h-full flex-col items-center justify-center gap-4">
-      {selectedMemberObject !== undefined ? (
-        <>
-          <div className="flex flex-col items-center justify-center gap-4 rounded-xl p-8 text-center text-slate-300 sm:w-[400px] sm:border sm:border-slate-300">
+      <div className="flex flex-col items-center justify-center gap-4 rounded-xl p-8 text-center text-slate-300 sm:w-[400px] sm:border sm:border-slate-300">
+        {selectedMemberObject !== undefined ? (
+          <>
             <img
               className="rounded-full"
               src={selectedMemberObject?.channelImage}
@@ -18,22 +18,28 @@ export default function Member(): React.JSX.Element {
               height="80px"
               width="80px"
             />
-            <h1 className="text-2xl">{selectedMemberObject?.channelName}</h1>
+            <h1
+              className={`text-2xl ${selectedMemberObject !== undefined ? 'opacity-100' : 'opacity-0'}`}
+            >
+              {selectedMemberObject?.channelName}
+            </h1>
             <MemberTable selectedMemberObject={selectedMemberObject} />
-          </div>
-          <Link className="text-slate-300 underline" to="/">
-            Back
-          </Link>
-        </>
-      ) : (
-        <>
-          <table className="table max-w-sm text-center text-sm sm:text-base">
-            <tbody>
-              <TableLoading />
-            </tbody>
-          </table>
-        </>
-      )}
+          </>
+        ) : (
+          <>
+            <div className="h-20 w-20 animate-pulse rounded-full bg-slate-700" />
+            <div className="h-6 w-[200px] animate-pulse rounded-full bg-slate-700" />
+            <table className="table max-w-sm text-center text-sm sm:text-base">
+              <tbody>
+                <TableLoading small />
+              </tbody>
+            </table>
+          </>
+        )}
+      </div>
+      <Link className="text-slate-300 underline" to="/">
+        Back
+      </Link>
     </div>
   );
 }
