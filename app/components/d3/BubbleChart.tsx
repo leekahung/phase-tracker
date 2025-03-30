3af3d40285b06a5194d670130e4698710a7b2ac2
Ka Hung Lee <lee.kahung414@gmail.com>
2025-03-30 00:56:44 -0700

    Update bubble chart to be clustered and interactive

--- app/components/d3/BubbleChart.tsx
+++ app/components/d3/BubbleChart.tsx
@@ -5,7 +5,7 @@ import LoadingChart from '../animation/LoadingChart';
 import { calculateFontSize } from '~/utils/d3Helpers';
 
 interface HierarchyNode {
-  name: string;
+  name?: string;
   generation?: string;
   value?: number;
   children?: HierarchyNode[];
@@ -43,64 +43,209 @@ export default function BubbleChart({ data }: Props) {
 
     const color = d3.scaleOrdinal().domain(generations).range(d3.schemeCategory10);
 
-    const pack = d3.pack<HierarchyNode>().size([width, height]);
-
     const inputObject: HierarchyNode = {
-      name: 'root',
-      children: generations.map((generation) => ({
-        name: generation,
-        children: groupByGen[generation].map((member) => ({
-          name: member.memberNameEn,
-          value: member.subscribers,
-          generation: member.generation,
-        })),
+      children: data.map((member) => ({
+        name: member.memberNameEn,
+        value: member.subscribers,
+        generation: member.generation,
       })),
     };
 
-    const hierarchy = d3.hierarchy(inputObject).sum((d) => d.value ?? 0);
+    const hierarchy = d3.hierarchy<HierarchyNode>(inputObject).sum((d) => d.value ?? 0);
+    const nodes = d3.pack<HierarchyNode>().size([width, height]).padding(1)(hierarchy).leaves();
 
-    const root = pack(hierarchy);
+    const simulation = d3
+      .forceSimulation(nodes)
+      .force('x', d3.forceX(width / 2).strength(0.01))
+      .force('y', d3.forceY(height / 2).strength(0.01))
+      .force('cluster', forceCluster())
+      .force('collide', forceCollide())
+      .force('bounds', forceBoundaries(width, height));
 
-    const nodes = root.descendants?.() ?? [];
-
-    const bubbles = svg
-      .selectAll('.bubble')
-      .data(nodes)
-      .enter()
+    const node = svg
       .append('g')
-      .attr('class', 'bubble')
-      .attr('transform', (d) => `translate(${d.x}, ${d.y})`);
+      .selectAll('circle')
+      .data(nodes)
+      .join('circle')
+      .attr('cx', (d) => d.x)
+      .attr('cy', (d) => d.y)
+      .attr('fill', (d) => color(d.data.generation || 'default') as string)
+      .call(drag(simulation));
 
-    bubbles
-      .filter((d) => d.depth === 1)
-      .append('circle')
-      .attr('r', (d) => d.r)
-      .attr('fill', (d) => color(d.data.name) as string)
-      .attr('opacity', 0.4);
+    node
+      .transition()
+      .delay(() => Math.random() * 500)
+      .duration(750)
+      .attrTween('r', (d) => {
+        const i = d3.interpolate(0, d.r);
+        return (t) => `${(d.r = i(t))}`;
+      });
 
-    bubbles
-      .filter((d) => d.depth === 2)
-      .append('circle')
-      .attr('r', (d) => d.r)
-      .attr('fill', (d) => color(d.parent?.data.name ?? '') as string);
+    const labelNamePart1 = svg
+      .append('g')
+      .selectAll('text')
+      .data(nodes)
+      .join('text')
+      .attr('dy', (d) => `-${calculateFontSize(d.r)}px`)
+      .attr('fill', 'white')
+      .attr('font-size', (d) => `${calculateFontSize(d.r)}px`)
+      .attr('width', (d) => `${d.r}px`)
+      .attr('text-anchor', 'middle')
+      .text((d) => `${d.data.name?.split(' ')[0]}`);
 
-    bubbles
-      .filter((d) => d.depth === 2)
-      .append('text')
-      .attr('dy', '-0.4em')
-      .style('text-anchor', 'middle')
-      .style('font-size', (d) => `${calculateFontSize(d.r) * 0.8}px`)
-      .text((d) => d.data.name);
+    const labelNamePart2 = svg
+      .append('g')
+      .selectAll('text')
+      .data(nodes)
+      .join('text')
+      .attr('dy', 0)
+      .attr('fill', 'white')
+      .attr('font-size', (d) => `${calculateFontSize(d.r)}px`)
+      .attr('width', (d) => `${d.r}px`)
+      .attr('text-anchor', 'middle')
+      .text((d) => `${d.data.name?.split(' ')[1]}`);
 
-    // Member subscriber count
-    bubbles
-      .filter((d) => d.depth === 2)
-      .append('text')
-      .attr('x', 0)
-      .attr('dy', '1.2em')
-      .style('font-size', (d) => `${calculateFontSize(d.r)}px`)
-      .text((d) => d.data.value?.toLocaleString() ?? 0);
+    const labelValues = svg
+      .append('g')
+      .selectAll('text')
+      .data(nodes)
+      .join('text')
+      .attr('dy', (d) => `${calculateFontSize(d.r) * 1.3}px`)
+      .attr('fill', 'white')
+      .attr('font-size', (d) => `${calculateFontSize(d.r)}px`)
+      .attr('text-anchor', 'middle')
+      .text((d) => `${d.data.value?.toLocaleString()}`);
+
+    simulation.on('tick', () => {
+      node.attr('cx', (d) => d.x).attr('cy', (d) => d.y);
+      labelNamePart1.attr('x', (d) => d.x).attr('y', (d) => d.y);
+      labelNamePart2.attr('x', (d) => d.x).attr('y', (d) => d.y);
+      labelValues.attr('x', (d) => d.x).attr('y', (d) => d.y);
+    });
   }, [height, width]);
 
   return <svg ref={chartRef} className="h-auto w-[90%] rounded-4xl bg-slate-500 sm:w-[70%]" />;
+}
+
+const drag = (simulation) => {
+  function dragstarted(event, d) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(event, d) {
+    d.fx = event.x;
+    d.fy = event.y;
+  }
+
+  function dragended(event, d) {
+    if (!event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+
+  const dragAction = d3.drag().on('start', dragstarted).on('drag', dragged).on('end', dragended);
+
+  return dragAction;
+};
+
+function centroid(nodes: d3.HierarchyCircularNode<HierarchyNode>[]): { x: number; y: number } {
+  let x = 0;
+  let y = 0;
+  let z = 0;
+  for (const d of nodes) {
+    const k = d.r ** 2;
+    x += d.x * k;
+    y += d.y * k;
+    z += k;
+  }
+  return { x: x / z, y: y / z };
+}
+
+function forceCluster() {
+  const strength = 0.2;
+  let nodes: d3.HierarchyCircularNode<HierarchyNode>[];
+
+  function force(alpha: number) {
+    const centroids = d3.rollup(nodes, centroid, (d) => d.data.generation);
+    const l = alpha * strength;
+    for (const d of nodes) {
+      const { x: cx, y: cy } = centroids.get(d.data.generation) as { x: number; y: number };
+      d.vx -= (d.x - cx) * l;
+      d.vy -= (d.y - cy) * l;
+    }
+  }
+
+  force.initialize = (_: d3.HierarchyCircularNode<HierarchyNode>[]) => (nodes = _);
+
+  return force;
+}
+
+function forceCollide() {
+  const alpha = 0.4;
+  const paddingWithinGroup = 2;
+  const paddingBetweenGroups = 4;
+  let nodes: d3.HierarchyCircularNode<HierarchyNode>[];
+  let maxRadius: number;
+
+  function force() {
+    const quadtree = d3.quadtree(
+      nodes,
+      (d) => d.x,
+      (d) => d.y
+    );
+    for (const d of nodes) {
+      const r = d.r + maxRadius;
+      const nx1 = d.x - r,
+        ny1 = d.y - r;
+      const nx2 = d.x + r,
+        ny2 = d.y + r;
+      quadtree.visit((q, x1, y1, x2, y2) => {
+        if (!q.length)
+          do {
+            if ('data' in q && q.data !== d) {
+              const r =
+                d.r +
+                q.data.r +
+                (d.data.generation === q.data.data.generation
+                  ? paddingWithinGroup
+                  : paddingBetweenGroups);
+              let x = d.x - q.data.x;
+              let y = d.y - q.data.y;
+              let l = Math.hypot(x, y);
+              if (l < r) {
+                l = ((l - r) / l) * alpha;
+                d.x -= x *= l;
+                d.y -= y *= l;
+                q.data.x += x;
+                q.data.y += y;
+              }
+            }
+          } while (q === q.next);
+        return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
+      });
+    }
+  }
+
+  force.initialize = (_: d3.HierarchyCircularNode<HierarchyNode>[]) =>
+    (maxRadius =
+      (d3.max((nodes = _), (d) => d.r) ?? 0) + Math.max(paddingWithinGroup, paddingBetweenGroups));
+
+  return force;
+}
+
+function forceBoundaries(width: number, height: number) {
+  let nodes: d3.HierarchyCircularNode<HierarchyNode>[];
+
+  function force() {
+    for (const d of nodes) {
+      d.x = Math.max(d.r, Math.min(width - d.r, d.x));
+      d.y = Math.max(d.r, Math.min(height - d.r, d.y));
+    }
+  }
+
+  force.initialize = (_: d3.HierarchyCircularNode<HierarchyNode>[]) => (nodes = _);
+
+  return force;
 }
--- app/routes/groupView.tsx
+++ app/routes/groupView.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import BubbleChart from '~/components/d3/BubbleChart';
+import RefreshIconSVG from '~/components/icons/RefreshIconSVG';
 import useChannels from '~/hooks/useChannels';
 
 export function meta() {
@@ -7,13 +9,24 @@ export function meta() {
 
 export default function groupView() {
   const { members, isError } = useChannels();
+  const [refresh, setRefresh] = useState(0);
 
   return isError ? (
     <div />
   ) : (
     <div className="flex h-full flex-col items-center justify-center gap-4">
-      <h1 className="text-3xl">Subscriber Count</h1>
-      <BubbleChart data={members} />
+      <div className="relative flex items-center justify-center">
+        <h2 className="text-lg sm:text-2xl">Subscriber Count</h2>
+        <button
+          className="absolute -right-12 flex h-10 w-10 cursor-pointer items-center justify-center rounded-full transition hover:rotate-90 hover:bg-slate-600"
+          type="button"
+          aria-label="refresh chart"
+          onClick={() => setRefresh((prev) => prev + 1)}
+        >
+          <RefreshIconSVG />
+        </button>
+      </div>
+      <BubbleChart data={members} key={refresh} />
     </div>
   );
 }
